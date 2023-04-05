14f83b206595f9f83bf9f0af69c159c5265611bb
VINAYCHARY07 <123128034+VINAYCHARY07@users.noreply.github.com>
2023-04-05 17:28:40 -0500

    commited

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var DoctorRouter = require('./routes/Doctor');
+var SelectorRouter = require('./routes/Selector');
+var boardRouter = require('./routes/board');
+
 
 var app = express();
 
@@ -21,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Doctor', DoctorRouter);
+app.use('/board', boardRouter);
+app.use('/Selector', SelectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
